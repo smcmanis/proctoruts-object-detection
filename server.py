ad83d6e08c96b635729a4cebbf6f39fce4d6e936
smcmanis <simon.mcmanis@gmail.com>
2021-10-20 10:46:22 +1100

    wow forgot to add server.py

--- server.py
+++ server.py
@@ -5,10 +5,10 @@ import os
 from PIL import Image
 from flask import Flask, request, Response
 
-app = Flask(__name__)
+myapp = Flask(__name__)
 
 # for CORS
-@app.after_request
+@myapp.after_request
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
@@ -16,22 +16,22 @@ def after_request(response):
     return response
 
 
-@app.route('/')
+@myapp.route('/')
 def index():
     return Response('Tensor Flow object detection')
 
 
-@app.route('/local')
+@myapp.route('/local')
 def local():
     return Response(open('./static/local.html').read(), mimetype="text/html")
 
 
-@app.route('/video')
+@myapp.route('/video')
 def remote():
     return Response(open('./static/video.html').read(), mimetype="text/html")
 
 
-@app.route('/test')
+@myapp.route('/test')
 def test():
     PATH_TO_TEST_IMAGES_DIR = 'object_detection/test_images'  # cwh
     TEST_IMAGE_PATHS = [os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 3)]
@@ -42,7 +42,7 @@ def test():
     return objects
 
 
-@app.route('/image', methods=['POST'])
+@myapp.route('/image', methods=['POST'])
 def image():
     try:
         image_file = request.files['image']  # get the image
@@ -64,9 +64,10 @@ def image():
         return e
 
 
-def start_app():
+
 	# without SSL
-    app.run(debug=True, host='0.0.0.0')
+    # myapp.run(debug=True, host='0.0.0.0')
 
 	# with SSL
-    #app.run(debug=True, host='0.0.0.0', ssl_context=('ssl/server.crt', 'ssl/server.key'))
+    #myapp.run(debug=True, host='0.0.0.0', ssl_context=('ssl/server.crt', 'ssl/server.key'))
+
